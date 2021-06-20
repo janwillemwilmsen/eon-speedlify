53045a3319efebd68adbbb7923317f1f97079f8f
essentonline <essent.online@essent.nl>
2021-06-20 22:55:19 +0200

    tweede-sites

---
+++ _data/sites/AAeon-de.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "Eon.de", // optional, falls back to object key
+	description: "Eon.de pages",
+	options: {
+		frequency: 60 * 168, 
+		// frequency: 1* 100, 
+		// 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.eon.de/de/pk/solar/solaranlage-kaufen.html",
+		"https://www.eon.de/de/pk/service/kontakt.html",
+		"https://www.eon.de/de/gk.html",
+		"https://www.eon.de/de/pk/strom/stromanbieter/guenstiger-stromanbieter.html",
+		"https://www.eon.de/de/pk/erdgas/erdgasanbieter-wechseln.html",
+		"https://www.eon.de/de/pk/heizung/waermepumpe.html",	 
+	]
+};
