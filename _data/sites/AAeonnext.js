e5977acadae23bceb18a3a769405e0342ad421d8
essentonline <essent.online@essent.nl>
2021-06-21 21:00:45 +0200

    eonnext-essent

---
+++ _data/sites/AAeonnext.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "Eonnext.com", // optional, falls back to object key
+	description: "Eonnext.com pages",
+	options: {
+		frequency: 60 * 168, 
+		// frequency: 1* 100, 
+		// 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.eonnext.com/policies/energy-efficiency",
+		"https://www.eonnext.com/tariffs",
+		"https://www.eonenergy.com/for-your-home/products-and-services",
+		"https://www.eonenergy.com/solar-panels/photovoltaic.html",
+		"https://www.eonenergy.com/boilers.html",
+		"https://www.eonnext.com/business",
+		"https://www.eonnext.com/dashboard/journey/quote",	 
+	]
+};
