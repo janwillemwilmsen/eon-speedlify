162e48bd754994ebd61ee388e27ef523293104ff
essentonline <essent.online@essent.nl>
2021-09-18 16:20:07 +0200

    funnels

---
+++ _data/sites/GGfunnel.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: "E/G Funnels", // optional, falls back to object key
+	description: "Elek en Gas funnels",
+	options: {
+		// frequency: 1 * 10, 
+		frequency: 60 * 168, 
+		// 24 hours
+		freshChrome: "run", // verschillende sites is RUN
+	},
+	urls: [
+		 
+		"https://www.essent.nl/beta/horizontal-offer",
+		"https://www.energiedirect.nl/beta/calculate",
+		"https://www.vattenfall.nl/producten/energie/",
+		"https://www.eneco.nl/duurzame-energie/",
+		"https://www.coolblue.nl/energie",
+		"https://www.greenchoice.nl/tarieven/",
+		"https://www.budgetthuis.nl/energie/",
+		"https://www.frankenergie.nl/",
+
+	]
+};
